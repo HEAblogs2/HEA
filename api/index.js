f6944fc67f1e41dd5a2a710a3d17b33692fcc789
houssem <houssemfraj5@gmail.com>
2022-12-10 21:59:22 +0000

    fixing

--- api/index.js
+++ api/index.js
@@ -8,18 +8,22 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const multer  = require('multer');
-const { response } = require("express");
+const multer = require('multer');
+
 // multer is a middelware for handling a multiple form of data. 
 // we use it here for handling the images uploaded 
 dotenv.config();
 app.use(express.json())
 
-mongoose.set('strictQuery', false);
+
+ mongoose.set('strictQuery', false);
 
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI,{
+
+   
+  })
   .then(() => {
     console.log('connected to db');
   })
@@ -29,24 +33,24 @@ mongoose
 
   const storage = multer.diskStorage({destination:(req,file,cb)=>{
     cb(null,"images")
-  } ,filename: (req, file, cb)=>{
-cb(null,req.body.name)
+  },filename: (req, file, cb)=>{
+cb(null,"hello.jpg")
   }
 });
 const upload =multer({storage:storage})
 //we gonna use the post method to upload a single 
-app.post("/api/upload",upload.single(),(req,res)=>{
-  response.status(201).json("file has been uploaded")
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+  res.status(200).send("file has been uploaded")
 })
 
     app.use("/api/auth",authRoute)
     app.use("/api/users",userRoute)
     app.use("/api/posts",postRoute)
-    app.use("/api/category",categoryRoute)
+    app.use("/api/categories",categoryRoute)
 
 
 
-app.listen("5001",()=>{
+app.listen("5002",()=>{
 
     console.log("backend is running ");
 });
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -32,22 +32,36 @@ res.status(500).json(err);
 
 //for the Login 
 
-router.post('/login',async (req,res)=>{
+// router.post('/login',async (req,res)=>{
 
-try{
+// try{
 
-const user = await User.findOne({username: req.body.username})
-!user && response.status(400).json("wrong username")
+// const user = await User.findOne({username: req.body.username})
+// !user && response.status(400).json("wrong username")
 
-const validated =await bcrypt.compare(req.body.password, user.password)
-!validated && response.status(400).json("wrong password")
+// const validated =await bcrypt.compare(req.body.password, user.password)
+// !validated && response.status(400).json("wrong password")
 
-response.status(200).json(user)
-}catch(err){
+// response.status(200).json(user)
+// }catch(err){
+//     res.status(500).json(err);
+// }
+
+// })
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
     res.status(500).json(err);
-}
-
-})
+  }
+});
 
 
 module.exports =router
